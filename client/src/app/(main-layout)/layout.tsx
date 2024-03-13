1db1ee3344ca619002ad58169e11e39b8aab52ce
Arian <liquinintity@gmail.com>
2024-03-13 14:42:49 +0200

    Implemented loader in context

--- client/src/app/(main-layout)/layout.tsx
+++ client/src/app/(main-layout)/layout.tsx
@@ -1,4 +1,6 @@
-import React, { useContext } from 'react';
+'use client';
+
+import React, { ReactNode } from 'react';
 import Sidebar from '@/src/components/common/Sidebar/Sidebar';
 import SecondarySidebar from '@/src/components/common/SecondarySidebar/SecondarySidebar';
 import styles from './layout.module.scss';
@@ -6,16 +8,23 @@ import Loader from '@/src/components/UI/Loader/Loader';
 import { LoaderContextProvider } from '@/src/contexts/LoaderContext';
 import useLoaderContext from '@/src/hooks/useLoaderContext';
 
-export default function MainLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  // const { isLoading } = useLoaderContext();
+export default function MainLayout({ children }: { children: ReactNode }) {
+  return (
+    <LoaderContextProvider>
+      <MainLayoutContent>{children}</MainLayoutContent>
+    </LoaderContextProvider>
+  );
+}
+
+const MainLayoutContent = ({ children }: { children: ReactNode }) => {
+  const { isLoading } = useLoaderContext();
+
   return (
     <main className={styles.layout}>
       <Sidebar />
+      {children}
       <SecondarySidebar />
+      {isLoading && <Loader />}
     </main>
   );
-}
+};
